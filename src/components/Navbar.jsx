5bc6219fae0bbb1963614360f06247616bf960c9
Marco Leskovar <leskovarmarco@gmail.com>
2024-04-15 22:06:34 -0300

    preentrega-01 V1

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,34 +1,35 @@
+import '../style/Navbar.css'
+import { CartWidget } from './CartWidget'
+
 export const Navbar = () => {
     return (
-        <div>
-            <nav className="navbar navbar-expand-lg bg-body-tertiary">
-                <div className="container-fluid">
-                    <a className="navbar-brand" href="#">Navbar</a>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
+        <div className="navbarContainer">
+            <nav className="navbar navbar-expand-lg">
+                <div className="menuContainer container-fluid">
+                    <a className="logo navbar-brand" href="#">
+                        <img className='logoImg' src="/images/full_logo.webp" alt="logo_empresa" />
+                    </a>
+                    <button className="menuBtn navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="menutBtnImg navbar-toggler-icon"></span>
                     </button>
-                    <div className="collapse navbar-collapse" id="navbarNavDropdown">
-                    <ul className="navbar-nav">
-                        <li className="nav-item">
-                        <a className="nav-link active" aria-current="page" href="#">Home</a>
-                        </li>
-                        <li className="nav-item">
-                        <a className="nav-link" href="#">Features</a>
-                        </li>
-                        <li className="nav-item">
-                        <a className="nav-link" href="#">Pricing</a>
-                        </li>
-                        <li className="nav-item dropdown">
-                        <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                            Dropdown link
-                        </a>
-                        <ul className="dropdown-menu">
-                            <li><a className="dropdown-item" href="#">Action</a></li>
-                            <li><a className="dropdown-item" href="#">Another action</a></li>
-                            <li><a className="dropdown-item" href="#">Something else here</a></li>
+                    <div className="itemContainer collapse navbar-collapse" id="navbarNavDropdown">
+                        <ul className="itemList navbar-nav">
+                            <li className="item nav-item">
+                                <a className="itemLink nav-link active" aria-current="page" href="#">Contactanos</a>
+                            </li>
+                            <li className="item nav-item">
+                                <a className="itemLink nav-link" href="#">Nosotros</a>
+                            </li>
+                            <li className="item nav-item dropdown">
+                                <a className="itemLink nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Productos</a>
+                                <ul className="itemDropdownContainer dropdown-menu">
+                                    <li className='itemDropdown'><a className="itemDropdownLink dropdown-item" href="#">Tornillos</a></li>
+                                    <li className='itemDropdown'><a className="itemDropdownLink dropdown-item" href="#">Tarugos</a></li>
+                                    <li className='itemDropdown'><a className="itemDropdownLink dropdown-item" href="#">Cintas Tramadas</a></li>
+                                </ul>
+                            </li>
                         </ul>
-                        </li>
-                    </ul>
+                        <CartWidget />
                     </div>
                 </div>
             </nav>
